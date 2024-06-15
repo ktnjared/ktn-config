3d1a6caf6e8240bd75127ef1f8cd1a6cec50008e
Jared Schmidt <email@jaredschmidt.net>
2024-06-14 23:52:44 -0400

    added in defaults; browser.newtabpage, browser.urlbar

--- Firefox Nightly/user.js
+++ Firefox Nightly/user.js
@@ -2,245 +2,266 @@
 // ║ Firefox Nightly user.js                                              ║
 // ╚══════════════════════════════════════════════════════════════════════╝
 
+// ╔═══════════════════════════════════════════════════════════════════════╗
+// ║ Nightly Experiments                                                   ║
+// ╚═══════════════════════════════════════════════════════════════════════╝
+
+user_pref('dom.webgpu.enabled', true); //true
+user_pref('layout.css.grid-template-masonry-value.enabled', true); //true
+
+// ╔═══════════════════════════════════════════════════════════════════════╗
+// ║ app.                                                                  ║
+// ╚═══════════════════════════════════════════════════════════════════════╝
+
+// Show fewer update prompts
+user_pref('app.update.suppressPrompts', true);
+
 // ╔══════════════════════════════════════════════════════════════════════╗
 // ║ browser.                                                             ║
 // ╚══════════════════════════════════════════════════════════════════════╝
 
+// Alt-Click / Option-Click to Save
+user_pref('browser.altClickSave', true); // false
+
 // Display warning when opening about:config
-// DEFAULT: true
-user_pref('browser.aboutConfig.showWarning', false);
+user_pref('browser.aboutConfig.showWarning', false); // true
 
 // Restore default bookmarks
-// DEFAULT: true
-user_pref('browser.bookmarks.restore_default_bookmarks', false);
+user_pref('browser.bookmarks.restore_default_bookmarks', false); // true
 
 // Show mobile bookmarks
-// DEFAULT: true
-user_pref('browser.bookmarks.showMobileBookmarks', false);
+user_pref('browser.bookmarks.showMobileBookmarks', false); // true
 
 // Backspace key behavior
-user_pref('browser.backspace_action', 0);
+user_pref('browser.backspace_action', 0); // ¯\_(ツ)_/¯
 
 // Blink tag or text-decoration: blink
-// DEFAULT: false
-user_pref('browser.blink_allowed', true);
+user_pref('browser.blink_allowed', true); // false
+
+// Content Blocking
+user_pref('browser.contentblocking.report.hide_vpn_banner', true); // false
+
+// New Tab
+user_pref('browser.newtabpage.activity-stream.discoverystream.ctaButtonSponsors', false); // true
+user_pref('browser.newtabpage.activity-stream.discoverystream.editorsPicksHeader.enabled', false); // true
+user_pref('browser.newtabpage.activity-stream.discoverystream.enabled', false); // true
+user_pref('browser.newtabpage.activity-stream.discoverystream.newSponsoredLabel.enabled', false); // true
+user_pref('browser.newtabpage.activity-stream.discoverystream.personalization.enabled', false); // true
+user_pref('browser.newtabpage.activity-stream.discoverystream.readTime.enabled', false); // true
+user_pref('browser.newtabpage.activity-stream.discoverystream.region-weather-config', null); // ¯\_(ツ)_/¯
+user_pref('browser.newtabpage.activity-stream.discoverystream.saveToPocketCard.enabled', false); // true
+user_pref('browser.newtabpage.activity-stream.discoverystream.sendToPocket.enabled', false); // true
+user_pref('browser.newtabpage.activity-stream.discoverystream.spocs.personalized', false); // true
+user_pref('browser.newtabpage.activity-stream.discoverystream.spocTopsitesPlacement.enabled', false); // true
+user_pref('browser.newtabpage.activity-stream.discoverystream.sponsored-collections.enabled', false); // true
+user_pref('browser.newtabpage.activity-stream.discoverystream.sponsored-collections.enabled', false); // true
+user_pref('browser.newtabpage.activity-stream.feeds.aboutpreferences', false); // true
+user_pref('browser.newtabpage.activity-stream.feeds.discoverystreamfeed', false); // true
+user_pref('browser.newtabpage.activity-stream.feeds.favicon', false); // true
+user_pref('browser.newtabpage.activity-stream.feeds.newtabinit', false); // true
+user_pref('browser.newtabpage.activity-stream.feeds.places', false); // true
+user_pref('browser.newtabpage.activity-stream.feeds.prefs', false); // true
+user_pref('browser.newtabpage.activity-stream.feeds.recommendationprovider', false); // true
+user_pref('browser.newtabpage.activity-stream.feeds.section.highlights', false); // true
+user_pref('browser.newtabpage.activity-stream.feeds.section.topstories.options', null); // ¯\_(ツ)_/¯
+user_pref('browser.newtabpage.activity-stream.feeds.sections', false); // true
+user_pref('browser.newtabpage.activity-stream.feeds.system.topsites', false); // true
+user_pref('browser.newtabpage.activity-stream.feeds.systemtick', false); // true
+user_pref('browser.newtabpage.activity-stream.feeds.telemetry', false); // true
+user_pref('browser.newtabpage.activity-stream.feeds.wallpaperfeed', false); // true
+user_pref('browser.newtabpage.activity-stream.feeds.weatherfeed', false); // true
+user_pref('browser.newtabpage.activity-stream.hideTopSitesTitle', true); // false
+user_pref('browser.newtabpage.activity-stream.impressionId', null); // ¯\_(ツ)_/¯ UUID
+user_pref('browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcuts.havePinned', null); // ¯\_(ツ)_/¯
+user_pref('browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcuts.searchEngines', null); // ¯\_(ツ)_/¯
+user_pref('browser.newtabpage.activity-stream.section.highlights.rows', 0); // 1
+user_pref('browser.newtabpage.activity-stream.section.topstories.rows', 0); // 1
+user_pref('browser.newtabpage.activity-stream.sectionOrder', null); // ¯\_(ツ)_/¯
+user_pref('browser.newtabpage.activity-stream.showSearch', false); // true
+user_pref('browser.newtabpage.activity-stream.showWeather', false); // true
+user_pref('browser.newtabpage.activity-stream.system.showSponsored', false); // true
+user_pref('browser.newtabpage.activity-stream.system.showWeather', false); // true
+user_pref('browser.newtabpage.activity-stream.telemetry.structuredIngestion.endpoint', null); // ¯\_(ツ)_/¯
+user_pref('browser.newtabpage.activity-stream.telemetry.ut.events', false); // true
+user_pref('browser.newtabpage.activity-stream.telemetry', false); // true
+user_pref('browser.newtabpage.activity-stream.topSitesRows', 0); // 1
+user_pref('browser.newtabpage.activity-stream.weather.display', null); // ¯\_(ツ)_/¯
+user_pref('browser.newtabpage.activity-stream.weather.locationSearchEnabled', false); // true
+user_pref('browser.newtabpage.activity-stream.weather.query', null); // ¯\_(ツ)_/¯
+user_pref('browser.newtabpage.activity-stream.weather.temperatureUnits', null); // f
+user_pref('browser.newtabpage.pinned', null); // ¯\_(ツ)_/¯
+
+// Tabs
+user_pref('browser.tabs.hoverPreview.enabled', false); // true
+user_pref('browser.tabs.hoverPreview.showThumbnails', false); // true
+
+// URL Bar
+user_pref('browser.urlbar.quicksuggest.impressionCaps.nonSponsoredEnabled', false); // true
+user_pref('browser.urlbar.quicksuggest.impressionCaps.sponsoredEnabled', false); // true
+user_pref('browser.urlbar.quicksuggest.nonSponsoredIndex', '-1'); // -1
+user_pref('browser.urlbar.quicksuggest.sponsoredIndex', '-1'); // -1
+user_pref('browser.urlbar.sponsoredTopSites', false); // true
+user_pref('browser.urlbar.suggest.pocket', false); // true
+user_pref('browser.urlbar.suggest.quicksuggest.nonsponsored', false); // true
+user_pref('browser.urlbar.suggest.quicksuggest.sponsored', false); // true
+user_pref('browser.urlbar.suggest.topsites', false); // true
+user_pref('browser.urlbar.suggest.trending', false); //true
+user_pref('browser.urlbar.suggest.weather', false); // true
+user_pref('browser.urlbar.suggest.yelp', false); // true
+user_pref('browser.urlbar.trimHttps', true); // true
+user_pref('browser.urlbar.trimURLs', true); // true
+user_pref('browser.urlbar.weather.featureGate', false); // true
+user_pref('browser.urlbar.weather.ignoreVPN', false); // true
+
+// VPN
+user_pref('browser.vpn_promo.enabled', false); // true
 
 // Press and hold Q to quit the browser
-// DEFAULT: true
-user_pref('browser.warnOnQuitShortcut', false);
-
-// Weather
-user_pref('browser.newtabpage.activity-stream.discoverystream.region-weather-config', null);
-user_pref('browser.newtabpage.activity-stream.feeds.weatherfeed', false);
-user_pref('browser.newtabpage.activity-stream.showWeather', false);
-user_pref('browser.newtabpage.activity-stream.system.showWeather', false);
-user_pref('browser.newtabpage.activity-stream.weather.locationSearchEnabled', false);
-user_pref('browser.newtabpage.activity-stream.weather.query', null);
-user_pref('browser.newtabpage.activity-stream.weather.temperatureUnits', null);
-user_pref('browser.urlbar.suggest.weather', false);
-user_pref('browser.urlbar.weather.featureGate', false);
-user_pref('browser.urlbar.weather.ignoreVPN', false);
+user_pref('browser.warnOnQuitShortcut', false); // true
 
 // ╔══════════════════════════════════════════════════════════════════════╗
 // ║ extensions.                                                          ║
 // ╚══════════════════════════════════════════════════════════════════════╝
 
 // Autofill addresses
-// DEFAULT: true
-user_pref('extensions.formautofill.addresses.enabled', false);
+user_pref('extensions.formautofill.addresses.enabled', false); // true
 
 // Autofill credit cards
-// DEFAULT: true
-user_pref('extensions.formautofill.creditCards.enabled', false);
+user_pref('extensions.formautofill.creditCards.enabled', false); // true
 
 // Pocket enabled
-// DEFAULT: true
-user_pref('extensions.pocket.enabled', false);
+user_pref('extensions.pocket.enabled', false); // true
 
 // Recommended Add-ons in about:addons
-// DEFAULT: true
-user_pref('extensions.htmlaboutaddons.recommendations.enabled', false);
+user_pref('extensions.htmlaboutaddons.recommendations.enabled', false); // true
 
 // Translations disabled
-// DEFAULT: true
-user_pref('extensions.translations.disabled', false);
+user_pref('extensions.translations.disabled', false); // true
 
 // ╔══════════════════════════════════════════════════════════════════════╗
 // ║ font.                                                                ║
 // ╚══════════════════════════════════════════════════════════════════════╝
 
 // Default font
-// DEFAULT: serif
-user_pref('font.default.x-western', 'san-serif');
+user_pref('font.default.x-western', 'san-serif'); // serif
 
 // Minimum font size
-// DEFAULT: 9
-user_pref('font.minimum-size.x-western', 11);
+user_pref('font.minimum-size.x-western', 11); // 9
 
 // monospace font
-// DEFAULT: Courier New
-user_pref('font.name.monospace.x-western', 'Comic Code Ligatures');
+user_pref('font.name.monospace.x-western', 'Comic Code Ligatures'); // Courier New
 
 // san-serif font
-// DEFAULT: Arial
-user_pref('font.name.sans-serif.x-western', 'SF Pro');
+user_pref('font.name.sans-serif.x-western', 'SF Pro'); // Arial
 
 // serif font
-// DEFAULT: Georgia
-user_pref('font.name.serif.x-western', 'New York');
+user_pref('font.name.serif.x-western', 'New York'); // Georgia
 
 // ╔══════════════════════════════════════════════════════════════════════╗
 // ║ geo.                                                                 ║
 // ╚══════════════════════════════════════════════════════════════════════╝
 
 // Geolocation enabled
-// DEFAULT: true
-user_pref('geo.enabled', false);
+user_pref('geo.enabled', false); // true
 
 // Geolocation provider to use
-// DEFAULT:
-user_pref('geo.wifi.uri', '');
+user_pref('geo.wifi.uri', ''); // ¯\_(ツ)_/¯
 
 // ╔══════════════════════════════════════════════════════════════════════╗
 // ║ image.                                                               ║
 // ╚══════════════════════════════════════════════════════════════════════╝
 
 // JPEG-XL support
-// DEFAULT: false
-user_pref('image.jxl.enabled', true);
+user_pref('image.jxl.enabled', true); // false
+
+// ╔═══════════════════════════════════════════════════════════════════════╗
+// ║ layout.                                                               ║
+// ╚═══════════════════════════════════════════════════════════════════════╝
+
+// Always underline links
+user_pref('layout.css.always_underline_links', true); // false
 
 // ╔══════════════════════════════════════════════════════════════════════╗
 // ║ middlemouse.                                                         ║
 // ╚══════════════════════════════════════════════════════════════════════╝
 
 // Paste with middlemouse click
-// DEFAULT:
-user_pref('middlemouse.paste', true);
+user_pref('middlemouse.paste', true); // false
 
 // Scroll with middlemouse
-// DEFAULT:
-user_pref('middlemouse.scrollbarPosition', true);
+user_pref('middlemouse.scrollbarPosition', true); // true
 
 // ╔══════════════════════════════════════════════════════════════════════╗
 // ║ network.                                                             ║
 // ╚══════════════════════════════════════════════════════════════════════╝
 
 // Disable DNS pre-fetching
-// DEFAULT: false
-user_pref('network.dns.disablePrefetch', true);
+user_pref('network.dns.disablePrefetch', true); // false
+user_pref('network.predictor.enabled', false); // true
+user_pref('network.prefetch-next', false); // true
 
-// DEFAULT: true
-user_pref('network.predictor.enabled', false);
-
-// DEFAULT: true
-user_pref('network.prefetch-next', false);
-
-// DEFAULT: null
-user_pref('network.trr.custom_uri', 'https://dns10.quad9.net/dns-query');
-
-// DEFAULT: null
-user_pref('network.trr.excluded-domains', 'ktn.one, nie.moe, dab-komodo.ts.net');
-
-// DEFAULT:
-user_pref('network.trr.mode', 3);
-
-// DEFAULT: https://mozilla.cloudflare-dns.com/dns-query
-user_pref('network.trr.uri', 'https://dns10.quad9.net/dns-query');
+// DNS over HTTPS
+user_pref('network.trr.custom_uri', 'https://dns10.quad9.net/dns-query'); // null
+user_pref('network.trr.default_provider_uri', 'https://dns10.quad9.net/dns-query'); // https://mozilla.cloudflare-dns.com/dns-query
+user_pref('network.trr.excluded-domains', 'ktn.one, nie.moe, dab-komodo.ts.net'); // null
+user_pref('network.trr.mode', 3); // ¯\_(ツ)_/¯
+user_pref('network.trr.uri', 'https://dns10.quad9.net/dns-query'); // https://mozilla.cloudflare-dns.com/dns-query
 
 // ╔══════════════════════════════════════════════════════════════════════╗
 // ║ notification.                                                        ║
 // ╚══════════════════════════════════════════════════════════════════════╝
 
 // Allow notifications
-// DEFAULT: true
-user_pref('notification.feature.enabled', false);
+user_pref('notification.feature.enabled', false); // true
 
 // ╔══════════════════════════════════════════════════════════════════════╗
 // ║ permissions.                                                         ║
 // ╚══════════════════════════════════════════════════════════════════════╝
 
-// DEFAULT:
-user_pref('permissions.default.desktop-notification', 2);
-
-// DEFAULT:
-user_pref('permissions.default.microphone', 2);
-
-// DEFAULT:
-user_pref('permissions.default.xr', 2);
-
-// DEFAULT: true
-user_pref('permissions.postPrompt.animate', false);
+user_pref('permissions.default.desktop-notification', 2); // ¯\_(ツ)_/¯
+user_pref('permissions.default.microphone', 2); // ¯\_(ツ)_/¯
+user_pref('permissions.default.xr', 2); // ¯\_(ツ)_/¯
+user_pref('permissions.postPrompt.animate', false); // true
 
 // ╔══════════════════════════════════════════════════════════════════════╗
 // ║ privacy.                                                             ║
 // ╚══════════════════════════════════════════════════════════════════════╝
 
-// DEFAULT: false
-user_pref('privacy.donottrackheader.enabled', true);
-
-// DEFAULT: false
-user_pref('privacy.fingerprintingProtection', true);
-
-// DEFAULT: false
-user_pref('privacy.globalprivacycontrol.enabled', true);
-
-// DEFAULT: false
-user_pref('privacy.globalprivacycontrol.was_ever_enabled', true);
-
-// DEFAULT:
+user_pref('privacy.donottrackheader.enabled', true); // false
+user_pref('privacy.fingerprintingProtection', true); // false
+user_pref('privacy.globalprivacycontrol.enabled', true); // false
+user_pref('privacy.globalprivacycontrol.was_ever_enabled', true); // false
 user_pref('privacy.purge_trackers.date_in_cookie_database', 0);
-
-// DEFAULT: false
-user_pref('privacy.sanitize.sanitizeOnShutdown', true);
-
-// DEFAULT: false
-user_pref('privacy.trackingprotection.emailtracking.enabled', true);
-
-// DEFAULT: false
-user_pref('privacy.trackingprotection.enabled', true);
-
-// DEFAULT: false
-user_pref('privacy.trackingprotection.socialtracking.enabled', true);
-
-// DEFAULT: false
-user_pref('privacy.userContext.enabled', true);
-
-// DEFAULT: false
-user_pref('privacy.userContext.ui.enabled', true);
+user_pref('privacy.sanitize.sanitizeOnShutdown', true); // false
+user_pref('privacy.trackingprotection.emailtracking.enabled', true); // false
+user_pref('privacy.trackingprotection.enabled', true); // false
+user_pref('privacy.trackingprotection.socialtracking.enabled', true); // false
+user_pref('privacy.userContext.enabled', true); // false
+user_pref('privacy.userContext.ui.enabled', true); // false
 
 // ╔══════════════════════════════════════════════════════════════════════╗
 // ║ reader.                                                              ║
 // ╚══════════════════════════════════════════════════════════════════════╝
 
-// DEFAULT:
-user_pref('reader.color_scheme', 'light');
-
-// DEFAULT:
-user_pref('reader.font_size', 8);
-
-// DEFAULT:
-user_pref('reader.line_height', 3);
+user_pref('reader.color_scheme', 'light'); // ¯\_(ツ)_/¯
+user_pref('reader.font_size', 8); // ¯\_(ツ)_/¯
+user_pref('reader.line_height', 3); // ¯\_(ツ)_/¯
 
 // ╔══════════════════════════════════════════════════════════════════════╗
 // ║ uc.                                                                  ║
 // ╚══════════════════════════════════════════════════════════════════════╝
 
-// DEFAULT: true
-user_pref('uc.tweak.disable-drag-space', false);
-
-// DEFAULT: true
-user_pref('uc.tweak.hide-tabs-bar', false);
+user_pref('uc.tweak.disable-drag-space', false); // true
+user_pref('uc.tweak.hide-tabs-bar', false); // true
 
 // ╔══════════════════════════════════════════════════════════════════════╗
 // ║ ui.                                                                  ║
 // ╚══════════════════════════════════════════════════════════════════════╝
 
-// DEFAULT:
-user_pref('ui.systemUsesDarkTheme', 0);
+user_pref('ui.systemUsesDarkTheme', 0); // ¯\_(ツ)_/¯
 
 // ╔══════════════════════════════════════════════════════════════════════╗
 // ║ xpinstall.                                                           ║
 // ╚══════════════════════════════════════════════════════════════════════╝
 
-// DEFAULT: true
-user_pref('xpinstall.signatures.required', false);
+user_pref('xpinstall.signatures.required', false); // true

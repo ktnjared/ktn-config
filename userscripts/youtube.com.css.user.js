f7a82bdfe4b877245cb0fd9b0c5dd9d029c637bc
Jared <git@jared.ktn.one>
2024-12-22 00:48:20 -0500

    migrated uBO cosmetic filters

--- userscripts/youtube.com.css.user.js
+++ userscripts/youtube.com.css.user.js
@@ -2,151 +2,174 @@
 // @name        [CSS] youtube.com
 // @description UserStyles for youtube.com
 // @author      ktnjared
-// @version     1.0.1
+// @version     1.0.2
 // @run-at      document-start
 // @grant       GM_addStyle
 // @icon        https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @homepageURL https://ktn.one/jared/userscripts/
 // @downloadURL https://ktn.one/jared/userscripts/raw/youtube.com.css.user.js
 // @updateURL   https://ktn.one/jared/userscripts/raw/youtube.com.css.user.js
-// @match       *://youtube.com/*
 // @match       *://*.youtube.com/*
-// @match       *://youtu.be/*
 // @match       *://*.youtube/*
+// @match       *://youtu.be/*
+// @match       *://youtube.com/*
 // ==/UserScript==
 
-// Sidebar Element: Hide Shorts
+// ╔═══════════════════════════════════════════════════════════════════════╗
+// ║ Sidebar                                                               ║
+// ╚═══════════════════════════════════════════════════════════════════════╝
+
+// ═══ TOP ═════════════════════════════════════════════════════════════════
+// Hide Shorts
 GM_addStyle(`
     ytd-guide-entry-renderer:has(a[title~="Shorts"]) {
-        display: none;
+        display:none;
     }
 `);
 
-// Sidebar Element: Hide YouTube Music
+// Hide YouTube Music
 GM_addStyle(`
     ytd-guide-entry-renderer:has(a[href*="music.youtube"]) {
-        display: none;
+        display:none;
     }
 `);
 
-// Sidebar Element: Hide History
+// ═══ YOU ═════════════════════════════════════════════════════════════════
+// Hide History
 GM_addStyle(`
     ytd-guide-entry-renderer:has(a[href="/feed/history"]) {
         display:none;
     }
 `);
 
-// Sidebar Element: Hide Your movies & TV
+// Hide Your movies & TV
 GM_addStyle(`
     ytd-guide-entry-renderer:has(a[title="Your movies & TV"]) {
         display:none;
     }
 `);
 
-// Sidebar Element: Hide Downloads
+// Hide Downloads
 GM_addStyle(`
     ytd-guide-entry-renderer:has(a[href="/feed/downloads"]) {
         display:none;
     }
 `);
 
-// Sidebar Element: Hide Your Clips
+// Hide Your Clips
 GM_addStyle(`
     ytd-guide-entry-renderer:has(a[href="/feed/clips"]) {
         display:none;
     }
 `);
 
-// Sidebar Element: Hide Liked Videos
+// Hide Liked Videos
 GM_addStyle(`
     ytd-guide-entry-renderer:has(a[href="/playlist?list=LL"]) {
         display:none;
     }
 `);
 
-// Sidebar Element: Hide Your Videos
+// Hide Your Videos
 GM_addStyle(`
     ytd-guide-entry-renderer:has(a[title="Your videos"]) {
         display:none;
     }
 `);
 
+// ═══ SUBSCRIPTIONS ═══════════════════════════════════════════════════════
+// Hide Channels with new videos (eg. blue dot). Only display Live channels
+GM_addStyle(`
+    ytd-guide-entry-renderer[line-end-style="dot"] {
+        display:none;
+    }
+`);
 
+// Hide "All channels"
+GM_addStyle(`
+    ytd-guide-entry-renderer:has(youtube.com##a[href="/feed/channels"]) {
+        display:none;
+    }
+`);
 
+// Hide "Show more"
+GM_addStyle(`
+    ytd-guide-entry-renderer:has(youtube.com##a[title="Show more"]) {
+        display:none;
+    }
+`);
 
-//  ! Hide dynamic header buttons
+// ═══ EXPLORE ═════════════════════════════════════════════════════════════
+// This hides the entire section
+GM_addStyle(`
+    #sections > ytd-guide-section-renderer:nth-child(3) {
+        display:none;
+    }
+`);
 
-//  youtube.com###scroll-container:upward(#header)
+// ═══ MORE FROM YOUTUBE ═══════════════════════════════════════════════════
+GM_addStyle(`
+    #sections > ytd-guide-section-renderer:nth-child(4) {
+        display:none;
+    }
+`);
 
-//   ! Hide related searches and related results from search results
+// ═══ BOTTOM ══════════════════════════════════════════════════════════════
+GM_addStyle(`
+    #sections > ytd-guide-section-renderer:nth-child(5) {
+        display:none;
+    }
+`);
 
-//  youtube.com##ytd-search ytd-item-section-renderer ytd-shelf-renderer
+// ═══ FOOTER ══════════════════════════════════════════════════════════════
+GM_addStyle(`
+    *[id*="footer"]:has(*[id="copyright"]) {
+        display:none;
+    }
+`);
 
-//  youtube.com##ytd-search ytd-item-section-renderer ytd-horizontal-card-list-renderer
+// ╔═══════════════════════════════════════════════════════════════════════╗
+// ║ CHANNEL & VIDEO PAGES                                                 ║
+// ╚═══════════════════════════════════════════════════════════════════════╝
 
-//   ! --- CHANNEL AND VIDEO PAGES ---
+// Hide Clip button
+GM_addStyle(`
+    button[aria-label="Clip"] {
+        display:none;
+    }
+`);
 
-//  ! Hide Clip button
+// Hide Download button
+GM_addStyle(`
+    button[aria-label="Download"] {
+        display:none;
+    }
+`);
 
-//  youtube.com##[aria-label="Clip"]
+// Hide Join button
+GM_addStyle(`
+    button[aria-label="Join this channel"] {
+        display:none;
+    }
+`);
 
-//  ! Hide Download button
+// Hide "More actions"/elipsis button
+GM_addStyle(`
+    button[aria-label="More actions"] {
+        display:none;
+    }
+`);
 
-//  youtube.com##[aria-label="Download"]
+// Hide Super Thanks button
+GM_addStyle(`
+    button[aria-label="Thanks"] {
+        display:none;
+    }
+`);
 
-//  ! Hide Join button
-
-//  youtube.com##[aria-label="Join this channel"]
-
-//  ! Hide "More actions"/elipsis button
-
-//  youtube.com##[aria-label="More actions"]
-
-//  ! Hide Super Thanks button
-
-//  youtube.com##[aria-label="Thanks"]
-
-//  ! Hide Comment count
-
-//  www.youtube.com##.ytd-comments-header-renderer.style-scope.count-text
-
-
-
-//   ! --- SIDEBAR ---
-
-//  www.youtube.com##.ytd-comments-header-renderer.style-scope.count-text
-
-//   ! --- SIDEBAR ---
-
-
-
-//   ! --- Subscriptions ---
-
-//  ! Hide Channels with new videos (eg. blue dot). Only display Live channels
-
-//  youtube.com##ytd-guide-entry-renderer[line-end-style="dot"]
-
-//  ! Hide "All channels"
-
-//  youtube.com##a[href="/feed/channels"]:upward(ytd-guide-entry-renderer)
-
-//  ! Hide "Show more"
-
-//  youtube.com##a[title="Show more"]:upward(ytd-guide-entry-renderer)
-
-//   ! Hide "Explore" section
-
-//  youtube.com##ytd-guide-section-renderer>h3>yt-formatted-string:has-text(/Explore/):upward(ytd-guide-section-renderer)
-
-//  ! Hide "More from YouTube" section
-
-//  youtube.com##ytd-guide-section-renderer>h3>yt-formatted-string:has-text(/More from YouTube/):upward(ytd-guide-section-
-    //    er)
-
-    //  ! Hide bottom section
-
-//  youtube.com##a[title="Send feedback"]:upward(ytd-guide-section-renderer)
-
-//  ! Hide "Footer" section
-
-//  youtube.com###copyright:upward(#footer)
+// Hide dislike button
+GM_addStyle(`
+    .style-scope.ytd-menu-renderer.force-icon-button.style-text {
+        display: none;
+    }
+`);

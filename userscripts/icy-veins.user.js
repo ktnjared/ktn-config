0ca90883d6545f2f9b2469a6ffee188b049c50dc
Jared <git@email.ktn.one>
2025-02-16 21:40:22 -0500

    standardized header; added !important

--- userscripts/icy-veins.user.js
+++ userscripts/icy-veins.user.js
@@ -1,23 +1,24 @@
 // ==UserScript==
-// @name        [ktn] Icy-Veins
-// @description Customizations for Icy-Veins
-// @author      ktnjared
-// @version     1.0.0
-// @run-at      document-start
-// @grant       GM_addStyle
+// @name        [ktn] icy-veins.com
+// @namespace   [ktn] icy-veins.com
+// @description [ktn] icy-veins.com
 // @icon        https://www.google.com/s2/favicons?sz=64&domain=icy-veins.com
 // @homepageURL https://ktn.one/jared/userscripts/
 // @downloadURL https://ktn.one/jared/userscripts/raw/icy-veins.user.js
 // @updateURL   https://ktn.one/jared/userscripts/raw/icy-veins.user.js
-// @match       *://icy-veins.com/*
+// @author      ktnjared
+// @version     1.0.0
+// @run-at      document-start
+// @grant       GM_addStyle
+// @match       *://*.icy-veins.com/*
 // ==/UserScript==
 
 // invert colors to generate "light mode"
 GM_addStyle(`
-    "html {filter: invert(100%);}"
+    "html {filter: invert(100%) !important;}"
 `);
 
 // un-invert images to ensure they show normal colors
 GM_addStyle(`
-    "img,svg,video {filter: invert(100%);}"
+    "img,svg,video {filter: invert(100%) !important;}"
 `);
